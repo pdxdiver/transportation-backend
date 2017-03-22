36f6c969e322558ca9ea905104b719c5a22caa2c
Tony Fuentes <leobender@gmail.com>
2017-03-21 20:59:35 -0700

    lets try travis

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-export PATH=$PATH:~/.local/bin
-./bin/getconfig.sh
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-gunicorn homelessAPI.wsgi:application -b :8000
+# export PATH=$PATH:~/.local/bin
+# ./bin/getconfig.sh
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
+# gunicorn homelessAPI.wsgi:application -b :8000
+
+
+cp /code/transDjango/transDjango/settings_local_example.py /code/transDjango/transDjango/settings_local.py
+/code/transDjango/manage.py migrate
+/code/transDjango/manage.py import_jsons
+/code/transDjango/manage.py jsonToCIPPoints
+/code/transDjango/manage.py jsonToCIPLines
+/code/transDjango/manage.py jsonToCIPPolygons
+#./manage.py runserver
+/code/transDjango/manage.py runserver 0.0.0.0:8000
